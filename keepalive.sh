b5574762fb77a01b550ab5884d6570a4c7c436ad
Agócs Dániel <agocs.daniel@coresystems.hu>
2022-01-25 15:11:49 +0100

    Logging keepalive, postfix config level

--- keepalive.sh
+++ keepalive.sh
@@ -5,6 +5,8 @@ do
 	EXPIRES_IN=$(python3 /scripts/oauth2.py "${STATE_INI}" auth)
 	POSTFIX_RELAY_AUTH_PASSWORD=$(python3 /scripts/oauth2.py "${STATE_INI}" token) /scripts/provision.sh
 	/usr/sbin/postfix reload
+	[ -z "$EXPIRES_IN" ] && EXPIRES_IN=30
+	echo Refreshing token in ${EXPIRES_IN} seconds
 	sleep ${EXPIRES_IN}
 done
 
